d9ae0d4a559af1d67ac11264e9425c9b85f0c2fe
ealcey244 <elacey244@gmail.com>
2022-03-02 20:36:47 -0500

    Add project files.

---
+++ RomanNumeralConvert/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConsoleUIs
+{
+    public class Program
+    {
+        public static Dictionary<string, int> RomanDict = new Dictionary<string, int>()
+        {
+            { "I", 1 },
+            { "V", 5 },
+            { "X", 10 },
+            { "L", 50 },
+            { "C", 100 },
+            { "D", 500 },
+            { "M", 1000 }
+        };
+
+        static void Main(string[] args)
+        {
+            string romanNumeral = GetInput();
+
+            int num = ConvertToNum(romanNumeral);
+
+            Console.WriteLine(num);
+
+        }
+
+        public static string GetInput()
+        {
+            Console.WriteLine("What Roman Numeral you would like to convert to a number: ");
+            string romanNumeral = Console.ReadLine();
+            return romanNumeral;
+        }
+
+        private static int ConvertToNum(string romanNumeral)
+        {
+            int num = 0;
+            foreach (char c in romanNumeral.ToUpper())
+            {
+                RomanDict.TryGetValue(c.ToString(), out int nextNum);
+                num += nextNum;
+            }
+
+            if (romanNumeral.Contains("IV") || romanNumeral.Contains("IX"))
+            {
+                num -= 2;
+            }
+            if (romanNumeral.Contains("XL") || romanNumeral.Contains("XC"))
+            {
+                num -= 20;
+            }
+            if (romanNumeral.Contains("CD") || romanNumeral.Contains("CM"))
+            {
+                num -= 200;
+            }
+            return num;
+        }
+    }
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwoSum
+{
+    internal class Program
+    {
+        //  Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+        //  You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+        //  You can return the answer in any order.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Array of ints: ");
+            //int[] nums = Console.ReadLine();
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+            return Array.Empty<int>();
+        }
+
+        public int[] JustThoughts(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int leftOver = target - nums[i];
+
+                //This is a problem if there are two equal values in the array
+                //Also a problem if the value doesn't exist in the array
+                //could also drop the value that was checked?
+                //Or simply ignore the value checked
+                if (Array.Find(nums, element => element == leftOver) + nums[i] == target && leftOver != nums[i])
+                {
+                    return new int[] { nums[i], leftOver };
+                }
+            }
+
+            return Array.Empty<int>();
+        }
+    }
+}
